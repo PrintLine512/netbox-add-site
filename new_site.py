48ecd4407db7662e6e909dcc8154c7d446c886a1
PrintLine512 <119358432+PrintLine512@users.noreply.github.com>
2024-04-17 23:05:39 +0300

    Update new_site.py

--- new_site.py
+++ new_site.py
@@ -19,7 +19,7 @@ class NewBranchScript(Script):
         description="Имя латиницей, из спец. символов \"-\""
     )
     tenant = ObjectVar(
-        model=Tenant
+        model=Tenants
     )
     manufacturer_router = ObjectVar(
         model=Manufacturer,
